f85df8269babc874381818fc2af24ca5720a3de0
FireSouls25 <david.castillofl@campusucc.edu.co>
2025-11-06 11:08:06 -0500

    mas cosas

--- autentication_enmanuel_sergio/urls.py
+++ autentication_enmanuel_sergio/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
+from .views import HomeView, RegisterView
 
 app_name = 'autentication_enmanuel_sergio'
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='autentication/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('autentication_enmanuel_sergio:login')), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- autentication_enmanuel_sergio/views.py
+++ autentication_enmanuel_sergio/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import View
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse_lazy('management:reserva_list'))
+        return redirect(reverse_lazy('autentication_enmanuel_sergio:login'))
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'autentication/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('autentication_enmanuel_sergio:login'))
+        return render(request, 'autentication/register.html', {'form': form})
